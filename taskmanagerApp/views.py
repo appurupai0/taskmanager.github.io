726f7aff991e1f9fe89b37254d69c7f2e8c72abb
DESKTOP-6LJVOB6\ksun0 <ksun0415canada@gmail.com>
2023-09-11 20:42:44 +0900

    t0001 ボタンのタスクの抽出とタスクごとのボタンの自動生成いったん完了（完成ではない）

--- taskmanagerApp/views.py
+++ taskmanagerApp/views.py
@@ -3,24 +3,28 @@ import re
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-
 def top(request):
-    # print("aq")
     if request.POST:
         input_task = request.POST.get('input_task').split('\n')
-        regex_pattern = r"^\d+\.\s*(\[.+?\]\s*.+?)\s*->\s+予定[:：]\s*(.+?)$"
+        regex_pattern = r"^\d+\.\s*(.+?)\s*->\s+予定[:：]\s*(.+?)$"
         task_list = []
         for task in input_task:
             if task.strip():
-                print('入力されました')
-                # matches = re.match(regex_pattern, task)
-                # task_data = {
-                #     'task_name': matches[0],
-                #     'task_hour': matches[1],
-                # }
-                # task_list.append(task_data)
+                matches = re.match(regex_pattern, task.replace('\r',''))
+                task_data = {
+                    'written_task_txt': matches[0],
+                    'task_name': matches[1],
+                    'task_hour': matches[2],
+                }
+                task_list.append(task_data)
             else:
                 print('マッチしてません')
-        # for task in task_list:
+        print(task_list)
+        
+        ctx = {
+            'task_list': task_list,
+        }
+        return render(request, "taskmanagerApp/index.html", ctx)
+
     return render(request, "taskmanagerApp/index.html")
 
